0a1fccacf62372b6c8e03171077e03b20361ca37
kaustuvprajapati <kaustuvprajapati@gmail.com>
2025-09-28 18:26:43 +0100

    server status check

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,13 +3,18 @@ set -e
 
 echo "Starting Node.js application..."
 
-# Ensure pm2 is in PATH
 export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.19.5/bin
 
 cd /var/www/nodejs-app
 npm install --omit=dev
 
-pm2 start app.js --name nodejs-app
-pm2 save
+if pm2 list | grep -q "nodejs-app"; then
+  echo "PM2 process already running, restarting..."
+  pm2 restart nodejs-app
+else
+  echo "PM2 process not running, starting..."
+  pm2 start app.js --name nodejs-app
+fi
 
+pm2 save
 echo "Application started"
