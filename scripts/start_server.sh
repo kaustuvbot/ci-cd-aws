917251830e54be64ffd194359fdddc6620adfde0
kaustuvprajapati <kaustuvprajapati@gmail.com>
2025-09-28 18:17:52 +0100

    pm2 path added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+set -e
+
 echo "Starting Node.js application..."
+
+# Ensure pm2 is in PATH
+export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.19.5/bin
+
 cd /var/www/nodejs-app
 npm install --omit=dev
+
 pm2 start app.js --name nodejs-app
 pm2 save
+
 echo "Application started"
